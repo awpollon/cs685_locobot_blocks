bf463b17be5c2691b0ae89c02f19306141975c14
locobot <locobot@locobot04.mesa.gmu.edu>
2022-11-12 17:46:40 -0600

    refactor to extend base class

--- main.py
+++ main.py
@@ -5,9 +5,7 @@ import math
 from interbotix_xs_modules.locobot import InterbotixLocobotXS
 
 
-class BlockBot:
-    def __init__(self) -> None:
-        self.locobot = InterbotixLocobotXS("locobot_" + os.environ["LOCOBOT_MODEL"], "mobile_" + os.environ["LOCOBOT_MODEL"])
+class BlockBot(InterbotixLocobotXS):
 
     def pick_up_block(self):
         if os.environ["LOCOBOT_MODEL"] == 'wx250s':
@@ -15,22 +13,23 @@ class BlockBot:
         else:
             z_move = -0.25
 
-        self.locobot.arm.go_to_home_pose()
-        self.locobot.gripper.open()
-        self.locobot.arm.set_ee_cartesian_trajectory(z=z_move)
-        self.locobot.gripper.close()
-        self.locobot.arm.go_to_home_pose()
+        self.arm.go_to_home_pose()
+        self.gripper.open()
+        self.arm.set_ee_cartesian_trajectory(z=z_move)
+        self.gripper.close()
+        self.arm.go_to_home_pose()
 
 
 def main():
-    bot = BlockBot()
-    bot.locobot.base.move(0, math.pi/8, 8)
-    bot.pick_up_block()
+    locobot = BlockBot("locobot_" + os.environ["LOCOBOT_MODEL"], "mobile_" + os.environ["LOCOBOT_MODEL"])
 
-    bot.locobot.base.move(0, -math.pi/8, 8)
-    bot.locobot.gripper.open()
+    locobot.base.move(0, math.pi/12, 12)
+    locobot.pick_up_block()
 
-    bot.locobot.arm.go_to_sleep_pose()
+    locobot.base.move(0, -math.pi/12, 12)
+    locobot.gripper.open()
+
+    locobot.arm.go_to_sleep_pose()
 
 
 if __name__ == '__main__':
