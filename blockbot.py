f98828c37e283a0da383f81285c60b29b270df9c
locobot <locobot@locobot04.mesa.gmu.edu>
2022-11-13 17:09:09 -0600

    refactor blockbot to use baseclass

--- blockbot.py
+++ blockbot.py
@@ -3,18 +3,19 @@ import rospy
 from apriltag_ros.msg import AprilTagDetectionArray
 from interbotix_xs_modules.locobot import InterbotixLocobotXS
 
-class BlockBot:
+
+class BlockBot(InterbotixLocobotXS):
     def __init__(self):
+        InterbotixLocobotXS.__init__()
         self.found_tag = False
         self.initialize_robot()
 
     def initialize_robot(self):
-        self.locobot = InterbotixLocobotXS("locobot_px100", "mobile_px100")
-        self.locobot.camera.move("pan", 0)
-        self.locobot.camera.move("tilt", 1)
-        self.locobot.arm.go_to_sleep_pose()
-        self.locobot.gripper.close()
-        self.locobot.gripper.open()
+        self.camera.move("pan", 0)
+        self.camera.move("tilt", 1)
+        self.arm.go_to_sleep_pose()
+        self.gripper.close()
+        self.gripper.open()
 
     def get_tag_data(self, data):
         filtered_tags = [det for det in data.detections if det.id[0] in [413, 91]]
@@ -25,13 +26,13 @@ class BlockBot:
                 print(tag.pose.pose.pose.position)
 
     def grab_block(self):
-        self.locobot.base.move(0, math.pi/20, 0.5)
-        self.locobot.arm.go_to_home_pose()
-        self.locobot.arm.set_ee_cartesian_trajectory(z=-0.25)
-        self.locobot.gripper.close()
-        self.locobot.arm.go_to_home_pose()
-        self.locobot.gripper.open()
-        self.locobot.arm.go_to_sleep_pose()
+        self.base.move(0, math.pi/20, 0.5)
+        self.arm.go_to_home_pose()
+        self.arm.set_ee_cartesian_trajectory(z=-0.25)
+        self.gripper.close()
+        self.arm.go_to_home_pose()
+        self.gripper.open()
+        self.arm.go_to_sleep_pose()
 
     def rotate_and_find_tag(self):
         rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.get_tag_data)
@@ -44,12 +45,13 @@ class BlockBot:
                 rospy.signal_shutdown("Found the block")
                 break
             elif num_rots > 0:
-                self.locobot.base.move(0, math.pi/20.0, 0.5)
+                self.base.move(0, math.pi/20.0, 0.5)
             num_rots += 1
 
     def execute_sequence(self):
         self.rotate_and_find_tag()
 
+
 if __name__ == "__main__":
-    blockbot = BlockBot()
+    blockbot = BlockBot("locobot_px100", "mobile_px100")
     blockbot.execute_sequence()
