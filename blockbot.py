ce951cc0a3d055d0ef35645a5a2784d7a6f3b55a
locobot <locobot@locobot05.mesa.gmu.edu>
2022-11-13 16:54:10 -0600

    Stabilized grabbing block

--- blockbot.py
+++ blockbot.py
@@ -13,6 +13,8 @@ class BlockBot:
         self.locobot.camera.move("pan", 0)
         self.locobot.camera.move("tilt", 1)
         self.locobot.arm.go_to_sleep_pose()
+        self.locobot.gripper.close()
+        self.locobot.gripper.open()
 
     def get_tag_data(self, data):
         filtered_tags = [det for det in data.detections if det.id[0] in [413, 91]]
@@ -20,6 +22,16 @@ class BlockBot:
             tag = filtered_tags[0]
             if -0.01 < tag.pose.pose.pose.position.x < 0.01:
                 self.found_tag = True
+                print(tag.pose.pose.pose.position)
+
+    def grab_block(self):
+        self.locobot.base.move(0, math.pi/20, 0.5)
+        self.locobot.arm.go_to_home_pose()
+        self.locobot.arm.set_ee_cartesian_trajectory(z=-0.25)
+        self.locobot.gripper.close()
+        self.locobot.arm.go_to_home_pose()
+        self.locobot.gripper.open()
+        self.locobot.arm.go_to_sleep_pose()
 
     def rotate_and_find_tag(self):
         rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.get_tag_data)
@@ -28,6 +40,7 @@ class BlockBot:
         while not rospy.is_shutdown():
             if self.found_tag:
                 print(f"Found the block after {num_rots} rotations")
+                self.grab_block()
                 rospy.signal_shutdown("Found the block")
                 break
             elif num_rots > 0:
