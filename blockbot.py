d5f2d487becdfd6d3d7679f4397190280839f02f
locobot <locobot@locobot05.mesa.gmu.edu>
2022-11-13 19:20:05 -0600

    Saving block's position

--- blockbot.py
+++ blockbot.py
@@ -7,6 +7,7 @@ from interbotix_xs_modules.locobot import InterbotixLocobotXS
 class BlockBot(InterbotixLocobotXS):
     def initialize_robot(self):
         self.found_tag = False
+        self.position = None
         self.camera.move("pan", 0)
         self.camera.move("tilt", 1)
         self.arm.go_to_sleep_pose()
@@ -20,16 +21,21 @@ class BlockBot(InterbotixLocobotXS):
             tag = filtered_tags[0]
             if -0.01 < tag.pose.pose.pose.position.x < 0.01:
                 self.found_tag = True
+                self.position = tag.pose.pose.pose.position
                 print(tag.pose.pose.pose.position)
 
     def grab_block(self):
-        self.base.move(0, math.pi/20, 0.5)
+        base_x = 0
+        if abs(self.position.z - 4.9) > 1:
+            base_x = 2 * abs(self.position.z - 4.9)
+        self.base.move(base_x, math.pi/20, 1)
         self.arm.go_to_home_pose()
         self.arm.set_ee_cartesian_trajectory(z=-0.25)
         self.gripper.close()
         self.arm.go_to_home_pose()
         self.gripper.open()
         self.arm.go_to_sleep_pose()
+        self.base.move(-base_x, 0, 1)
 
     def rotate_and_find_tag(self):
         rospy.Subscriber("/tag_detections", AprilTagDetectionArray, self.get_tag_data)
@@ -42,7 +48,7 @@ class BlockBot(InterbotixLocobotXS):
                 rospy.signal_shutdown("Found the block")
                 break
             elif num_rots > 0:
-                self.base.move(0, math.pi/20.0, 0.5)
+                self.base.move(0, math.pi/18.0, 0.5)
             num_rots += 1
 
     def execute_sequence(self):
