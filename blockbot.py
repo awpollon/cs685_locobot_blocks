faf2544e15ca490e924c8558375b30f2ed9e0eb1
locobot <locobot@locobot04.mesa.gmu.edu>
2022-11-20 22:20:56 -0600

    execute seq, sucessful test

--- blockbot.py
+++ blockbot.py
@@ -22,7 +22,7 @@ LANDMARK_TAGS = [680, 681, 682, 683, 684, 86]
 BIN_TAG = 413
 TAGS = [*BLOCK_TAGS, *LANDMARK_TAGS, BIN_TAG]
 
-ROTATION_INCREMENT = math.pi/20.0
+ROTATION_INCREMENT = math.pi/16.0
 MOVE_INCREMENT = 0.03
 
 CAMERA_SETTINGS = {"tilt": 1, "pan": 0, "height": 0.45}
@@ -64,7 +64,7 @@ def calc_angle_dist(theta_1, theta_2):
 
 
 class BlockBot(InterbotixLocobotXS):
-    def __init__(self, align_camera=False) -> None:
+    def __init__(self, align_camera=True) -> None:
         super().__init__("locobot_px100", "mobile_px100")
         rospy.Subscriber("/tag_detections",
                          AprilTagDetectionArray, self.get_tag_data)
@@ -88,7 +88,6 @@ class BlockBot(InterbotixLocobotXS):
         # fix any erroneous gripper positions
         self.gripper.close()
         self.gripper.open()
-        
 
     def reset_base_pose(self):
         '''Resets pose estimate, odometry, and GTSAM data. 
@@ -150,7 +149,7 @@ class BlockBot(InterbotixLocobotXS):
 
     def grab_block(self):
         self.action_state = RobotActionState.PICK_UP_BLOCK
-        self.base.move(0, 2*ROTATION_INCREMENT, 1)
+        self.move(0, 2*ROTATION_INCREMENT, 1)
         self.arm.go_to_home_pose()
         self.arm.set_ee_cartesian_trajectory(z=-0.25)
         self.gripper.close()
@@ -160,9 +159,9 @@ class BlockBot(InterbotixLocobotXS):
     def release_block(self):
         self.action_state = RobotActionState.RELEASE_BLOCK
         self.arm.go_to_home_pose()
-        self.base.move(0.15, 0, 1)
+        self.move(0.15, 0, 1)
         self.gripper.open()
-        self.base.move(-0.15, 0, 1)
+        self.move(-0.15, 0, 1)
         self.arm.go_to_sleep_pose()
         self.action_state = RobotActionState.WAIT
 
@@ -175,15 +174,15 @@ class BlockBot(InterbotixLocobotXS):
                 pos = self.bin_position
 
             if not pos:  # No block in view
-                self.base.move(0, 2 * ROTATION_INCREMENT, 0.5)
+                self.move(0, 2 * ROTATION_INCREMENT, 0.5)
             else:  # Block in view
                 if GRABBABLE_MARGIN[0] < pos.x < GRABBABLE_MARGIN[1]: # Block in grabbable margin
                     if abs(pos.z - GRABBABLE_APRILTAG_Z) > 0.01:  # Block is far away
-                        self.base.move(MOVE_INCREMENT, 0, 0.5)
+                        self.move(MOVE_INCREMENT, 0, 0.5)
                     else:  # Block is grabbable
                         return True
                 else:  # Block not in grabbable margin
-                    self.base.move(
+                    self.move(
                         0, ROTATION_INCREMENT if pos.x < 0 else -ROTATION_INCREMENT, 0.5)
         self.action_state = RobotActionState.WAIT
 
@@ -199,7 +198,7 @@ class BlockBot(InterbotixLocobotXS):
             if (self.controller.goal_reached):
                 print("Goal reached")
                 self.__command(0, 0)
-                return
+                return True
 
             x_vel, theta_vel = self.controller.step(self.estimated_pose)
             self.__command(x_vel, theta_vel)
@@ -207,6 +206,8 @@ class BlockBot(InterbotixLocobotXS):
         self.action_state = RobotActionState.WAIT
         if i+1 == MAX_MOVES:
             print(f"Moves limit reached:{MAX_MOVES}")
+
+        return False
 
     def __command(self, x_vel, theta_vel):
         print(f'Velocities: {x_vel} {theta_vel}')
@@ -218,8 +219,9 @@ class BlockBot(InterbotixLocobotXS):
     def execute_sequence(self):
         if self.find_goal("block"):
             self.grab_block()
-            if self.find_goal("bin"):
+            if self.move_to_goal((0, 0, -math.pi)):
                 self.release_block()
+                self.move_to_goal()
 
 
 if __name__ == "__main__":
